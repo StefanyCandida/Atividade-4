75a3b47312ac4715900b0887b0c72a8d74ecf477
StefanyCandida <stefany.candida@gmail.com>
2022-01-21 16:45:00 -0300

    atividade 4

---
+++ script.js
@@ -0,0 +1,32 @@
+
+//1 - crie uma função que exiba uma mensagem no console
+function inicio(oi){
+    console.log(`Olá, vou me apresentar!`);
+}
+inicio(`inicio`);
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+function nomes(Stefany){
+    console.log(`Vamos do  ${Stefany}`);
+}
+nomes(`principio,`);
+//3 - crie uma função que receba um nome, um número, e um estilo musical (parâmetros) e exiba no console
+
+function estilo(nome, number, music){
+    console.log(`Meu nome é ${nome}, minha idade é ${number} e meu estilo de musica favorita é ${music}.`);
+}
+estilo(`Stefany`, `32`, `rock`);
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function media(film, music){
+    console.log(`Meu filme favorito é ${film}, minha musica favorita é ${music} .`);
+}
+media(`Valente`, `16-Legião`);
+
+
+//5 - crie uma função que retorne o triplo do número recebido no (parâmetro)
+
+
+
+
